b10f5ed5bee49f1bda4fb637fdb0f351d196c7d9
Nicholas Solana <solananicholas@gmail.com>
2022-01-31 00:47:48 -0500

    challenge

--- scraping.py
+++ scraping.py
@@ -19,6 +19,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres": hemispheres(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -96,6 +97,31 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    
+    hemisphere_image_urls = []
+    
+    links = browser.find_by_css('a.product-item img')
+
+    for i in range(len(links)):
+    
+        #create empty dictionary to store img and title for each hemisphere
+        hemisphere={}
+    
+        browser.find_by_css('a.product-item img')[i].click()
+    
+        sample_elem = browser.links.find_by_text('Sample').first
+        hemisphere['img_url'] = sample_elem['href']
+    
+        hemisphere['title'] = browser.find_by_css('h2.title').text
+    
+        hemisphere_image_urls.append(hemisphere)
+    
+        browser.back()
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
